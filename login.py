9734f1cdb0c44de3834ea44df029d2614e7085be
Jugalcody <jugalnerist@gmail.com>
2021-12-31 14:15:15 +0530

    Login credentials gui project

--- login.py
+++ login.py
@@ -6,26 +6,31 @@ name=tk.Label(text="user id : ")
 name.place(relx=0.13,rely=0.4)
 
 name_entry=tk.Entry()
-name_entry.place(relx=0.36,rely=0.4)
+name_entry.place(relx=0.18,rely=0.4)
 
 
 pas=tk.Label(text="password : ")
 pas.place(relx=0.12,rely=0.45)
 
 pas_entry=tk.Entry()
-pas_entry.place(relx=0.36,rely=0.45)
-
+pas_entry.place(relx=0.18,rely=0.45)
 f=open("word.txt","a+")
-                                              
-                                            
 
-#store={"jugal":"#python#"}
+import mysql.connector as mycon
+my=mycon.connect(user="jugal",passwd="Jugal2002@" ,host="localhost")
+cursor=my.cursor(buffered="True")
+cursor.execute("create database if not exists logindb")
+cursor.execute("use logindb")
+cursor.execute("create table if not exists logindb ( sno varchar(100)primary key,name varchar(100)unique key,password varchar(100))")                                             
+                                           
+
+store={"jugal":"#python#"}
 def click():
        name=name_entry.get()
        pas=pas_entry.get()
-    
-       if name=="jugal" :
-                   if pas=="#python#":
+       
+       for i in range(len(store)):
+                    if store[name]==pas:
                                  rr=messagebox.askquestion("user exist!!","Do you want to continue?")
                                  if rr=="yes":
                                              
@@ -46,10 +51,6 @@ def click():
                                                     v=c.get()
                                                     oo=t.get()
                                                     
-                                            
-                                            
-                                            
-                                            
                                                     f.write(v+"      "+oo+"\n")
                                                         
                                             tk.Button(text="Submit",command=submit,bg="skyblue").place(relx=0.09,rely=0.75)
@@ -58,8 +59,39 @@ def click():
                                               
                                             
 
-                   else:
+                    else:
                                  h=tk.Label(text="Wrong password").place(relx=0.14,rely=0.34)
+                                 
+
+                             
+name1=tk.Label(text="user id : ")
+name1.place(relx=0.50,rely=0.40) 
+name2_entry=tk.Entry()
+name2_entry.place(relx=0.55,rely=0.40)
+pas3=tk.Label(text="password : ")
+pas3.place(relx=0.50,rely=0.45)
+#cursor.execute("insert into logindb values(1,'jugal','#python#')")
+pas4_entry=tk.Entry()
+pas4_entry.place(relx=0.55,rely=0.45)
+def ok():
+    na=name2_entry.get();
+    p=pas4_entry.get();
+    cursor.execute("select * from logindb order by sno desc limit 1")
+    
+    h=cursor.fetchone()
+    if h==None:
+           b=1      
+    else:
+    
+           n=list(h)
+           b=int(n[0])+1
+    c="insert into logindb values(%s,%s,%s)"
+    o=str(b),str(na),str(p)
+    cursor.execute(c,o)
+    my.commit()
+y=tk.Button(text="Create new account",command=ok)
+y.place(relx=0.55,rely=0.50)
+                                 
                   
                    
      
@@ -67,7 +99,7 @@ def click():
        
 
 button=tk.Button(text="login",command=click)
-button.place(relx=0.15,rely=0.5)
+button.place(relx=0.15,rely=0.50)
 
 root.mainloop()
 f.close()
